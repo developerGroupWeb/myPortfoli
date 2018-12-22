f548ab72f576803bb3688767283792fff79c2106
DScode51 <fordavidsmithcode@gmail.com>
2018-12-22 21:37:24 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,366 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<title>My Portfolio</title>
+	<!-- Latest compiled and minified CSS -->
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Poppins:300,400,500,700" rel="stylesheet">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
+	<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	<!--My css -->
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+
+<body>
+	<section id="home-screen">
+		<video autoplay muted loop id="myVideo" class="d-none d-md-block">
+		  <source src="videos/FallyIpupa.mp4" type="video/mp4">
+		</video>
+
+		<div class="home-screen__content">
+			<div class="nav-bg">
+				<div class="container">
+					<nav class="navbar navbar-expand-md">
+					  <a class="navbar-brand font-weight-bold" href="#" style="font-size: 25px;">UA:</a>
+					  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+					    <span style="color: #fff;"><i class="fa fa-bars" aria-hidden="true"></i></span>
+					  </button>
+
+					  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+					    <ul class="navbar-nav ml-auto">
+					      <li class="nav-item active">
+					        <a class="nav-link" href="#home-screen">Accueil</a>
+					      </li>
+					      <li class="nav-item">
+					        <a class="nav-link" href="#about">About</a>
+					      </li>
+					      <li class="nav-item">
+					        <a class="nav-link" href="#skills">Skills</a>
+					      </li>
+					      <li class="nav-item">
+					      	<a href="#portfolio" class="nav-link">Portfolio</a>
+					      </li>
+					      <li class="nav-item">
+					        <a class="nav-link" href="#contact">Contact</a>
+					      </li>
+					  </div>
+					</nav>
+				</div>
+			</div>
+			
+			<div class="container home-content">
+				<div class="row">
+					<div class="col-md-7 col-sm-12">
+						<h1 class="wow fadeInDown font-weight-bold">I'm Designer & Front-end Web Developer.</h1>
+						<div class="barBlock wow fadeInRight"></div>
+						<h3 class="wow fadeInDown">Let's build something together...</h3>
+						<a href="#portfolio" class="btn wow fadeInRight">See my work</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section id="about" class="bg-danger">
+		<div class="container py-4">
+			<div class="row">
+				<h1 class="mx-auto wow fadeInDown my-5 font-weight-bold">About me</h1>
+			</div>
+
+			<div class="row">
+				<div class="mx-auto avatar wow fadeInDown">
+					<div>
+						<img src="images/P80911175658.jpg" alt="avatar" class="img-fluid" style="box-shadow: 0 0 1px 11px rgba(255, 173, 51,.15),0 0 1px 22px rgba(255, 173, 51,.1); border-radius: 100%;">
+					</div>		
+				</div>
+			</div>
+			<div class="row">
+				<h3 class="mx-auto text-center my-5 col-sm-8 offset-sm-2 blockquote wow fadeInDown">" I'm Uriel, Front-end Web Developer and User interface Designer from Ukraine and I love what I do. "</h3>
+			</div>
+		</div>
+	</section>
+
+	<section id="working-process" class="working-process text-center">
+		<div class="container py-4">
+			<div class="row">
+				<h1 class="mx-auto wow fadeInDown my-5">My Working Process</h1>
+			</div>
+			<div class="row mb-5">
+				<div class="col-md-3 col-sm-6 col-6 wow fadeInRight">
+					<i class="fa fa-arrows-alt fa-2x" aria-hidden="true"></i>
+					<h5 class="mt-2">PLAN</h5>
+				</div>
+
+				<div class="col-md-3 col-sm-6 col-6 wow fadeInUp">
+					<i class="fa fa-desktop fa-2x" aria-hidden="true"></i>
+					<h5 class="mt-2">DESIGN</h5>
+				</div>
+
+				<div class="col-md-3 col-sm-6 col-6 wow fadeInUp">
+					<i class="fa fa-file-code-o fa-2x" aria-hidden="true"></i>
+					<h5 class="mt-2">DEVELOPMENT</h5>
+				</div>
+
+				<div class="col-md-3 col-sm-6 col-6 fa-2x wow fadeInLeft">
+					<i class="fa fa-paper-plane" aria-hidden="true"></i>
+					<h5 class="mt-2">LAUNCH</h5>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section id="skills">
+		<div class="skills-cards">
+			<div class="container py-4">
+				<h1 class="text-center font-weight-bold wow fadeInDown my-5">My Skills</h1>
+				<div class="row">
+					<div class="col-12">
+						<div class="row">
+							<div class="col-md-4 col-sm-6">
+								<div class="card card2 wow fadeInUp">
+									<div class="card-face front text-center text-dark">
+										<h3 class="font-weight-bold mt-5">Web development</h3>
+										<div class="mt-5"><i class="fa fa-laptop fa-5x" aria-hidden="true"></i></div>
+									</div>
+									<div class="card-face back text-center p-4">
+										<h4>Front-end Developer</h4>
+										<h6 class="mt-2 font-weight-bold">Languages I speak:</h6>
+										<p>HTML, Pug, CSS, Sass, Less, Jquery, Javascript</p>
+										<h6 class="mt-2 font-weight-bold">Dev Tools:</h6>
+										<p>Sublim text, WebStorm, Bootstrap, Bulma, Git/Github, Bitbucket, Terminal</p>
+									</div>
+								</div>
+							</div>
+
+							<div class="col-md-4 col-sm-6">
+								<div class="card card1 wow fadeInDown">
+									<div class="card-face front text-center text-dark">
+										<h3 class="font-weight-bold mt-5">UI/UX Design</h3>
+										<div class="mt-5"><i class="fa fa-lightbulb-o fa-5x" aria-hidden="true"></i></div>
+									</div>
+									<div class="card-face back text-center p-4">
+										<h4>Intuitive</h4>
+										<h6>Strong preference for easy to use, intuitive UX/UI.</h6>
+									</div>
+								</div>
+							</div>
+
+							<div class="col-md-4 col-sm-6 offset-sm-3 offset-md-0">
+								<div class="card card3 wow fadeInDown">
+									<div class="card-face front text-center text-dark">
+										<h3 class="font-weight-bold mt-5">Pixel Perfetct</h3>
+										<div class="mt-5"><i class="fa fa-puzzle-piece fa-5x" aria-hidden="true"></i></div>
+									</div>
+									<div class="card-face back text-center p-4">
+										<h4>Fun</h4>
+										<h6>I just like make every design in pixel perfect.</h6>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="skills-bars">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-5 order-last order-lg-first">
+						<div class="row mt-5">
+							<div class="col-10 offset-1">
+								<div class="row mx-auto">
+									<div class="col-6">
+										<img src="images/html5.png" class="img-fluid w-50">
+									</div>
+									<div class="col-6">
+										<img src="images/CSS3_icon-icons.com_67069.svg" class="img-fluid w-50 d-block ml-auto">
+									</div>
+
+									<div class="col-6 mt-5">
+										<img src="images/bootstrap_stack.png" class="img-fluid w-50">
+									</div>
+									<div class="col-6 mt-5">
+										<img src="images/icon_square_big.png" class="img-fluid w-50 d-block ml-auto">
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+
+					<div class="col-lg-7 order-first order-lg-last">
+			          <div class="row px-4">
+			            <div class="progress w-100" style="height: 32px;">
+			            	<h6 class="bg-primary py-2 h-100 w-25 text-center">HTML</h6>
+			              	<div class="progress-bar bg-info wow fadeInLeft" style="width: 70%;" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+			              	<h6 class="ml-auto py-2 pr-2 d-none d-md-block">90%</h6>
+			            </div>
+			            
+			            <div class="progress w-100 mt-3" style="height: 32px;">
+			            	<h6 class="bg-primary py-2 px-4 h-100 w-25 text-center">CSS</h6>
+			              	<div class="progress-bar bg-info wow fadeInLeft" role="progressbar" style="width: 60%" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+			              	<h6 class="ml-auto py-2 pr-2 d-none d-md-block">80%</h6>
+			            </div>
+			            
+			            <div class="progress w-100 mt-3" style="height: 32px;">
+			            	<h6 class="bg-primary py-2 px-4 h-100 w-25 text-center">Bootstrap</h6>
+			              	<div class="progress-bar bg-info wow fadeInLeft" role="progressbar" style="width: 70%" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+			              	<h6 class="ml-auto py-2 pr-2 d-none d-md-block">90%</h6>
+			            </div>
+
+			            <div class="progress w-100 mt-3" style="height: 32px;">
+			            	<h6 class="bg-primary py-2 px-4 h-100 w-25 text-center">Jquery</h6>
+			              	<div class="progress-bar bg-info wow fadeInLeft" role="progressbar" style="width: 45%" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+			              	<h6 class="ml-auto py-2 pr-2 d-none d-md-block">55%</h6>
+			            </div>
+
+			            <div class="progress w-100 mt-3" style="height: 32px;">
+			            	<h6 class="bg-primary py-2 px-4 h-100 w-25 text-center">Javascript</h6>
+			              	<div class="progress-bar bg-info wow fadeInLeft" role="progressbar" style="width: 35%" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+			              	<h6 class="ml-auto py-2 pr-2 d-none d-md-block">45%</h6>
+			            </div>
+
+			            <div class="progress w-100 mt-3" style="height: 32px;">
+			            	<h6 class="bg-primary py-2 px-4 h-100 w-25 text-center">Web-pack</h6>
+			              	<div class="progress-bar bg-info wow fadeInLeft" role="progressbar" style="width: 50%" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+			              	<h6 class="ml-auto py-2 pr-2 d-none d-md-block">60%</h6>
+			            </div>
+
+			            <div class="progress w-100 mt-3" style="height: 32px;">
+			            	<h6 class="bg-primary py-2 px-4 h-100 w-25 text-center">Photoshop</h6>
+			              	<div class="progress-bar bg-info wow fadeInLeft" role="progressbar" style="width: 50%" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+			              	<h6 class="ml-auto py-2 pr-2 d-none d-md-block">60%</h6>
+			            </div>
+			          </div>
+			        </div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section id="portfolio">
+		<div class="container py-4">
+			<div class="row">
+		    	<h1 class="mx-auto mt-5 mb-4 font-weight-bold wow fadeInDown">Projects</h1>
+		    </div>
+		    <div class="wow fadeInRight" style="border: 2px solid #0099ff; width: 100px; margin: auto; margin-bottom: 60px;"></div>
+			<div class="row mb-5">
+				<div class="col-md-4 col-10 offset-1 offset-md-0 content-over">
+					<div class="row">
+						<img class="img-fluid w-100" src="images/portfolio1.jpg">
+						<div class="overlay">
+							<h4 class="text-center font-weight-bold mt-3 mb-5">La Royale News</h4>
+							<div class="mt-5 text-center btn-getOnline">
+								<a href=""><span class="btn bg-light mr-3" title="See online"><i class="fa fa-external-link" aria-hidden="true"></i></span></a>
+
+								<a href=""><span class="btn bg-dark" title="See on Github"><i class="fa fa-github" aria-hidden="true"></i></span></a>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4 col-10 offset-1 offset-md-0 my content-over">
+					<div class="row">
+						<img class="img-fluid w-100" src="images/portfolio2.jpg">
+						<div class="overlay">
+							<h4 class="text-center font-weight-bold mt-3 mb-5">Afri-competences plus</h4>
+							<div class="mt-5 text-center btn-getOnline">
+								<a href=""><span class="btn bg-light mr-3" title="See online"><i class="fa fa-external-link" aria-hidden="true"></i></span></a>
+
+								<a href=""><span class="btn bg-dark" title="See on Github"><i class="fa fa-github" aria-hidden="true"></i></span></a>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4 col-10 offset-1 offset-md-0 content-over">
+					<div class="row">
+						<img class="img-fluid w-100" src="images/portfolio3.jpg">
+						<div class="overlay">
+							<h4 class="text-center font-weight-bold mt-3 mb-5">JackPortfolio</h4>
+							<div class="mt-5 text-center btn-getOnline">
+								<a href=""><span class="btn bg-light mr-3" title="See online"><i class="fa fa-external-link" aria-hidden="true"></i></span></a>
+
+								<a href=""><span class="btn bg-dark" title="See on Github"><i class="fa fa-github" aria-hidden="true"></i></span></a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section id="contact">
+		<div class="container pt-4 pb-5">
+			<h1 class="text-center wow fadeInDown my-5">Get in touch</h1>
+			<div class="wow fadeInRight" style="border: 2px solid #0099ff; width: 100px; margin: auto; margin-bottom: 60px;"></div>
+			<div class="row">
+				<div class="col-md-6 wow fadeInLeft">
+					 <form action="" method="post" id="get_in_touch">
+					 	<h4 class="text-center" style="color: #0099ff">Have a question or want to work together?</h4>
+					 	<div class="form-group">
+					    <label for="name"></label>
+					    <input type="text" placeholder="your name" class="form-control" id="name">
+					  </div> 
+					  <div class="form-group">
+					    <label for="email"></label>
+					    <input type="email" placeholder="Email" class="form-control" id="email">
+					  </div>
+					  <div class="form-group">
+					    <label for="mssg"></label>
+					    <textarea type="text" placeholder="message" class="form-control" id="pwd"></textarea>
+					  </div>
+					  <button type="submit" name="submit" id="get_in_touch" class="btn col-sm-4">SEND</button>
+					</form> 
+				</div>
+				<div class="col-md-6 wow fadeInRight">
+					<div class="contact-data bg px-4 pb-2">
+		              <h5 class="pt-4">ADDRESS</h5>
+		              <p class="py-2">Dnipro, UKRAINE</p>
+			        </div>
+			        <div class="my-3 bg px-4 pb-2">
+			              <h5 class="pt-4">PHONE</h5>
+			              <p class="py-2">+380 66 215 36 47</p>
+			        </div>
+			        <div class="bg px-4 pb-2">
+			              <h5 class="pt-4">WEB</h5>
+			              <p class="pt-2">Mail: fordavidsmithcode@gmail.com</p>
+			              <p>Website: <a href="">www.davidsmith.io</a></p>
+			        </div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<footer>
+		<div class="container footer-end">
+			<div class="row">
+				<div class="col-sm-6 col-12">
+					Copyright 2018 - All rights reserved
+				</div>
+				
+				<div class="col-sm-6 col-12 ml-auto">
+					<div class="row">
+						<div class="ml-auto mr-2">
+							<b style="color: #0099ff;"><.UA></b>, made with <a href="http://getbootstrap.com/docs/4.1/" target="_blank">Booststrap</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</footer>
+
+	<a href="" class="back-to-top"><i class="fa fa-chevron-up"></i></a>
+
+<script src="https://code.jquery.com/jquery-3.3.1.js"
+  integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60="
+  crossorigin="anonymous"></script>
+<!--Wow js-->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+<!--My js-->
+<script type="text/javascript" src="js/functions.js"></script>
+</body>
+</html>
